d0a7546843b4c3f56e417e5d7b04191f24aabf81
Luuk van Houdt <luuk@vhoudt.nl>
2016-05-25 14:41:54 +0200

    Practicum 2

---
+++ src/P2/Deck.java
@@ -0,0 +1,13 @@
+package P2;
+
+public class Deck {
+    private String test;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+}
---
+++ src/P2/P2Main.java
@@ -0,0 +1,7 @@
+package P2;
+
+public class P2Main {
+    public static void main(String[] args) {
+
+    }
+}
